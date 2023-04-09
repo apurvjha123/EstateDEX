a61c5f1da8a0325b67f78bc6a50d5f1b5d6888cd
unknown <apurvjha123@gmail.com>
2023-04-09 13:44:01 +0530

    ..

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -27,23 +27,6 @@ async function main() {
   console.log(register)
 }
 
-// async function verify(contractAddress, args) {
-// const verify = async (contractAddress, args) => {
-//   console.log("Verifying contract...");
-//   try {
-//     await run("verify:verify", {
-//       address: contractAddress,
-//       constructorArguments: args,
-//     });
-//   } catch (e) {
-//     if (e.message.toLowerCase().includes("already verified")) {
-//       console.log("Already Verified!");
-//     } else {
-//       console.log(e);
-//     }
-//   }
-// };
-
 //main
 main()
   .then(() => process.exit(0))
